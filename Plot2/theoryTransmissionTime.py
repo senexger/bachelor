f3a3054bfe6bb8c41545e1f3eabb94a04fbebb07
Maximilian Gotthardt <senexger@gmail.com>
2022-02-23 02:58:07 +0100

    Add transimmsion time of bc

--- Plot2/theoryTransmissionTime.py
+++ Plot2/theoryTransmissionTime.py
@@ -2,6 +2,7 @@
 from codecs import backslashreplace_errors
 import numpy as np
 import matplotlib.pyplot as plt
+from pylab import *
 
 # setup data
 x  = np.arange(1,11,1)
@@ -10,29 +11,30 @@ x2 = np.arange(20,420,20)
 uc_transmissiontime_20byte        = np.zeros((10), dtype=int)
 bc_transmissiontime_20byte        = np.zeros((10), dtype=int)
 bc_transmissiontime_200byte_const = np.zeros((10), dtype=int)
-bc_transmissiontime_up_to_400     = np.zeros((20), dtype=int)
+# bc_transmissiontime_up_to_400     = np.zeros((20), dtype=int)
 
 for i in x:
     uc_transmissiontime_20byte[i-1]        = 940 * i
     bc_transmissiontime_20byte[i-1]        = (28+20*i)*8 + 50 + 144 +48
     bc_transmissiontime_200byte_const[i-1] = (28+200)*8 + 50 + 144 +48
 
-second_bc = 0
-for i in x2:
-    if (220 == i):
-        second_bc = second_bc + 1
-    bc_transmissiontime_up_to_400[int(i/20)-1] = (28+20*i/20)*8 + 50 + 144 +48 + ((28+200)*8 + 50 + 144 +48)*second_bc
+# second_bc = 0
+# for i in x2:
+#     if (220 == i):
+#         second_bc = second_bc + 1
+#     bc_transmissiontime_up_to_400[int(i/20)-1] = (28+20*i/20)*8 + 50 + 144 +48 + ((28+200)*8 + 50 + 144 +48)*second_bc
 
-#%% debug printing
 print(uc_transmissiontime_20byte)
 print(bc_transmissiontime_20byte)
 print(bc_transmissiontime_200byte_const)
-print(bc_transmissiontime_up_to_400)
+# print(bc_transmissiontime_up_to_400)
 
 #%%
 def plot_bc20_vs_bc200(ax):
-    ax.plot(x, uc_transmissiontime_20byte/1000 , ''  'bs-', label='Unicast', markerfacecolor='none', linewidth=1, color='forestgreen')
-    ax.plot(x, bc_transmissiontime_20byte/1000, 'r^-', label='Broadcast', markerfacecolor='none', linewidth=1, color='darkmagenta')
+    # with nice boxes
+    # ax.plot(x, uc_transmissiontime_20byte/1000, 'bs-', label='Unicast', markerfacecolor='none', linewidth=1, color='forestgreen')
+    ax.plot(x, uc_transmissiontime_20byte/1000, '-', label='Unicast', markerfacecolor='none', linewidth=1.3, color='forestgreen')
+    ax.plot(x, bc_transmissiontime_20byte/1000, '-', label='Broadcast', markerfacecolor='none', linewidth=1.3, color='darkmagenta')
     ax.set_xlabel('Number of WES')
     ax.set_ylabel('Transmission Time in μs')
     ax.legend(loc='best', frameon=False)
@@ -51,7 +53,14 @@ fig.tight_layout()
 #%% Broadcast increasing up to 400 Byte
 
 def plot_bc_up_to_200(ax):
-    ax.plot(x2, bc_transmissiontime_up_to_400/1000, 'rs-', label='Broadcast', markerfacecolor='none', linewidth=1, color='darkmagenta')
+    bc_hack = np.zeros((11))
+    for i in range(0,10):
+        bc_hack[i+1] = bc_transmissiontime_20byte[i]
+    bc_hack[0] = 474
+
+    ax.plot(x2[:10], bc_transmissiontime_20byte/1000, label='Broadcast', markerfacecolor='none', linewidth=1.3, color='darkmagenta')
+    ax.plot(x2[9:],  bc_hack/1000 + 2.066,                               markerfacecolor='none', linewidth=1.3, color='darkmagenta')
+
     ax.set_xlabel('Payload in Byte')
     ax.set_ylabel('Total Transmission Time in μs')
     ax.legend(loc='best', frameon=False)
