34e025e22ab3d8425e06aa3de8acffc4059c892e
Maximilian Gotthardt <senexger@gmail.com>
2022-03-09 16:25:32 +0100

    Add broadcast rr=1 functionality

--- Plot2/successRatio.py
+++ Plot2/successRatio.py
@@ -21,13 +21,56 @@ data_formatted = genfromtxt('/home/walther/Documents/bachelor/Plot2/removeme.csv
 # print(data_formatted.shape)
 
 # sanitize data to 3D array
-data = np.zeros((SEQ_SIZE, FIX_SIZE, EXP_SIZE))
+data = np.zeros((SEQ_SIZE, FIX_SIZE, EXP_SIZE), dtype=int)
 print(data.shape)
 
 for exp in range(0,EXP_SIZE):
     for fix in range(0,FIX_SIZE):
         for seq in range(0,SEQ_SIZE):
             data[seq,fix,exp] = data_formatted[seq+SEQ_SIZE*exp,fix]   
+
+#%% Success Ratio Rapid Repetition
+RR=6
+
+# shrink data to rr = 1
+# Merges for each rr all transmissions of the same sequence to 1 or 0
+data_rr = np.zeros((RR,100,FIX_SIZE,EXP_SIZE),dtype=int)
+
+for exp in range(0,EXP_SIZE):
+    for fix in range(0,FIX_SIZE):
+        for seq in range(0,int(SEQ_SIZE/6)):
+            for rr in range(0,RR):
+                # TODO Ugliest code ever!
+                if (rr==0):
+                    data_rr[rr,seq,fix,exp] = data[seq,fix,exp]
+                if (rr==1):
+                    if (data[2*seq, fix, exp] or data[2*seq+1, fix, exp]):
+                        data_rr[rr,seq,fix,exp] = 1
+                if (rr==2):
+                    if (data[3*seq, fix, exp] or data[3*seq+1, fix, exp] or data[3*seq+2, fix, exp]):
+                        data_rr[rr,seq,fix,exp] = 1
+                if (rr==3):
+                    if (data[4*seq, fix, exp] or data[4*seq+1, fix, exp] or data[4*seq+2, fix, exp] or data[4*seq+3, fix, exp]):
+                        data_rr[rr,seq,fix,exp] = 1
+                if (rr==4):
+                    if (data[5*seq, fix, exp] or data[5*seq+1, fix, exp] or data[5*seq+2, fix, exp] or data[5*seq+3, fix, exp] or data[5*seq+4, fix, exp]):
+                        data_rr[rr,seq,fix,exp] = 1
+                if (rr==5):
+                    if (data[6*seq, fix, exp] or data[6*seq+1, fix, exp] or data[6*seq+2, fix, exp] or data[6*seq+3, fix, exp] or data[6*seq+4, fix, exp] or data[6*seq+5, fix, exp]):
+                        data_rr[rr,seq,fix,exp] = 1
+                if (rr==6):
+                    if (data[7*seq, fix, exp] or data[7*seq+1, fix, exp] or data[7*seq+2, fix, exp] or data[7*seq+3, fix, exp] or data[7*seq+4, fix, exp] or data[7*seq+6, fix, exp] or data[7*seq+6, fix, exp]):
+                        data_rr[rr,seq,fix,exp] = 1
+
+
+sr_array = np.zeros((RR, EXP_SIZE,FIX_SIZE))
+
+for exp in range(0,EXP_SIZE):
+    for fix in range(0,FIX_SIZE):
+        for rr in range(0,RR):
+            count_is_zero = np.count_nonzero(data_rr[rr,:,fix,exp]==0)
+            sr_array[rr,exp,fix] = 100 - count_is_zero
+
 
 # print(data[:,:,-1])
 #%%
@@ -39,6 +82,7 @@ def sr_per_fixture_broadcast(data):
         for fix in range(0,FIX_SIZE):
             count_is_zero = np.count_nonzero(data[:,fix,exp]==0)
             sr_array[exp,fix] = 100* (1 - count_is_zero/SEQ_SIZE)
+            # print(1- count_is_zero/SEQ_SIZE)
 
     ax1.set_xlabel('Fixture enum.', color='black')
     ax1.set_ylabel('Success Ratio in %') 
@@ -48,7 +92,7 @@ def sr_per_fixture_broadcast(data):
     plt.figure(num=None, figsize=None, dpi=800)
     plt.show()
     
-    fig.savefig('Graphs/SR_per_fixture_broadcast.pdf', bbox_inches='tight') 
+    # fig.savefig('Graphs/SR_per_fixture_broadcast.pdf', bbox_inches='tight') 
     plt.close()
 
 flierprop_purp = dict(marker='s', markerfacecolor='purple', markersize=3)
@@ -77,16 +121,48 @@ def sr_per_fixture_unicast():
 fig, ax1 = plt.subplots()
 sr_per_fixture_unicast()
 
-#%% Success Ratio Rapid Repetition
 
-# shrink data to rr = 1
-data_rr1 = np.zeros((int(SEQ_SIZE/2),FIX_SIZE,EXP_SIZE),dtype=int)
+#%%
+#success ratio for node 3 broadcast with rapid repetition
 
-for exp in range(0,EXP_SIZE):
-    for fix in range(0,FIX_SIZE):
-        for seq in range(0,int(SEQ_SIZE/2)):
-            if (data[2*seq, fix, exp] or data[2*seq+1, fix, exp]):
-                data_rr1[seq,fix,exp] = 1
+def sr_per_node_bc_rr(array):
 
-print(data[0:20 ,3,1])
-print(data_rr1[0:10,3,1])
+    ax1.set_xlabel('Fixture enum.', color='black')
+    ax1.set_ylabel('Success Ratio in %') 
+    
+    # ax1.boxplot(sr_array[0,::10,:], flierprops=flierprop_purp, medianprops=medianprops_purp)
+    ax1.boxplot(sr_array[1,::10,:], flierprops=flierprop_purp, medianprops=medianprops_purp)
+
+    plt.figure(num=None, figsize=None, dpi=799)
+    plt.show()
+    
+    fig.savefig('Graphs/SR_per_fixture_rr1.pdf', bbox_inches='tight') 
+    plt.close()
+
+flierprop_purp = dict(marker='s', markerfacecolor='purple', markersize=3)
+medianprops_purp = dict(linestyle='-', color='purple')
+
+fig, ax1 = plt.subplots()
+sr_per_node_bc_rr(sr_array)
+#%%
+#success ratio for node 3 broadcast with rapid repetition
+
+def sr_per_node_bc_rr(array):
+
+    ax1.set_xlabel('Fixture enum.', color='black')
+    ax1.set_ylabel('Success Ratio in %') 
+    
+    # ax1.boxplot(sr_array[0,::10,:], flierprops=flierprop_purp, medianprops=medianprops_purp)
+    ax1.boxplot(sr_array[1,::10,:], flierprops=flierprop_purp, medianprops=medianprops_purp)
+
+    plt.figure(num=None, figsize=None, dpi=799)
+    plt.show()
+    
+    fig.savefig('Graphs/SR_per_fixture_rr1.pdf', bbox_inches='tight') 
+    plt.close()
+
+flierprop_purp = dict(marker='s', markerfacecolor='purple', markersize=3)
+medianprops_purp = dict(linestyle='-', color='purple')
+
+fig, ax1 = plt.subplots()
+sr_per_node_bc_rr(sr_array)
